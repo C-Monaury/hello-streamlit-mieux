3a692e7d6bd0a07711a34544fb7f08e030713bbb
Clément MONAURY <monaury.clement@gmail.com>
2023-12-14 23:42:10 +0100

    Modification des pages

---
+++ pages/Projet_Secret.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+
+
+st.link_button("The P Project","https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley")
+
---
+++ pages/Scenario.py
@@ -0,0 +1,14 @@
+
+
+from typing import Any
+
+import numpy as np
+
+import streamlit as st
+from streamlit.hello.utils import show_code
+
+
+st.title("Prévisions du futur")
+st.write("Dans cette selection nous allons tester différents scénarios pour lutter contre notre maladie")
+
+st.link_button("Aide visuelle","https://docs.streamlit.io/library/api-reference")
